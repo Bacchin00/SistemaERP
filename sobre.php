4af9e742a1c8e8a0ebf2ce1b7568de2f1bb3c06d
Victor Bacchin <victor.bacchin@fatec.sp.gov.br>
2024-10-08 19:03:10 -0300

    Primeira Versão Site ERP

---
+++ sobre.php
@@ -0,0 +1,25 @@
+<?php
+
+session_start(); 
+
+	include_once("cabec.php");
+?>
+
+    <p>&nbsp;</p>
+
+	<h2 align="center"><?php echo $lng['sobreSistema']; ?></h2>
+
+	<p>&nbsp;</p>
+	<p>&nbsp;</p>
+
+	<h3 align="center"><?php echo $lng['desenvolvido']; ?></h3>
+
+	<p>&nbsp;</p>
+    <p>&nbsp;</p>
+
+	<h3 align="center"><?php echo $lng['contato']; ?></h3>
+
+<?php
+    include_once("rodape.php"); 
+
+?>
